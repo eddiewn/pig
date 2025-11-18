33c29f5c36af576e35fd61f83ae48e560f578903
Eddie Wiik Nilsson <eddie.wiiknilsson@gmail.com>
2025-11-18 14:11:20 +0100

    Bare minimum game done, everything works.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const players = [];
-let playerIdTurn = 1;
+let playerTurn = 0;
 let numPlayers = 2;
 
 //Fisher-Yates Shuffle Algorithm
@@ -94,12 +94,32 @@ const createGameUI = () => {
     gameSection.id = 'game-section';
     
     const playerTurnH2 = document.createElement('h2');
-    playerTurnH2.textContent = `Player ${players[playerIdTurn].name}'s Turn`;
+    playerTurnH2.textContent = `Player ${players[playerTurn].name}'s Turn`;
     gameSection.append(playerTurnH2);
+
+    const playerCurrentScore = document.createElement('p');
+    playerCurrentScore.id = 'player-current-score';
+    playerCurrentScore.textContent = `Current Turn Score: ${players[playerTurn].turnScore}`;
+    gameSection.append(playerCurrentScore);
 
     const rollButton = document.createElement('button');
     rollButton.textContent = 'Roll Dice';
     gameSection.append(rollButton);
+
+    rollButton.addEventListener('click', () => {
+        const rollResult = roll();
+        players[playerTurn].turns += 1;
+
+        if (rollResult === 1) {
+            players[playerTurn].turnScore = 0;
+            playerTurn = (playerTurn + 1) % players.length;
+        } else{
+            players[playerTurn].turnScore += rollResult;
+        }
+
+        game();
+        checkGameState();
+    });
 
     const endTurnButton = document.createElement('button');
     endTurnButton.textContent = 'End Turn';
@@ -107,7 +127,58 @@ const createGameUI = () => {
 
     
 
+    endTurnButton.addEventListener('click', () => {
+        players[playerTurn].totalScore += players[playerTurn].turnScore;
+        players[playerTurn].turnScore = 0;
+        playerTurn = (playerTurn + 1) % players.length;
+        game();
+    });
+
+    const terminateGameButton = document.createElement('button');
+    terminateGameButton.textContent = 'End Game';
+
+    terminateGameButton.addEventListener('click', () => {
+        if(confirm("Are you sure??")){
+            players.length = 0;
+            playerTurn = 0;
+            preGame();
+        }
+    });
+
+    gameSection.append(terminateGameButton);
     main.append(gameSection);
+}
+
+const checkGameState = () => {
+    console.log("Checking Game State");
+    console.log(players[playerTurn].totalScore);
+        if ((players[playerTurn].totalScore + players[playerTurn].turnScore) >= 100) {
+            console.log("Game is over")
+            endGame(players[playerTurn]);
+            return;
+        }
+    return false;
+}
+
+const endGame = (winner) => {
+    console.log(winner)
+    const main = document.querySelector('main');
+    main.innerHTML = "";
+
+    const winnerAnnouncement = document.createElement('h1');
+    winnerAnnouncement.textContent = `${winner.name} Wins!`;
+
+    playAgainButton = document.createElement('button');
+    playAgainButton.textContent = 'Play Again';
+    playAgainButton.addEventListener('click', () => {
+        players.length = 0;
+        playerTurn = 0;
+        preGame();
+    });
+
+    winnerAnnouncement.append(playAgainButton);
+    main.append(winnerAnnouncement);
+    
 }
 
 const game = () => {
@@ -117,12 +188,7 @@ const game = () => {
 
     createGameScoreboard();
     createGameUI();
-
-
-
-
-    console.log(players)
-
+    console.log(players);
 }
 
 
